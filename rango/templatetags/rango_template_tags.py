5a46051731a3ccfc8cc53580e8d3f7e6a1b7c0c9
nikalinov <nikitakalinovskij@gmail.com>
2024-02-07 23:35:35 +0000

    end of chapter 8

---
+++ rango/templatetags/rango_template_tags.py
@@ -0,0 +1,11 @@
+from django import template
+from rango.models import Category
+
+
+register = template.Library()
+
+
+@register.inclusion_tag('rango/categories.html')
+def get_category_list(category=None):
+    return {'categories': Category.objects.all(),
+            'current_category': category}
