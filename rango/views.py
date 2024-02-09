e32d438c719df6147af3aa4825c0b536881c8284
nikalinov <nikitakalinovskij@gmail.com>
2024-02-09 17:14:08 +0000

    end of chapter 10. All tests pass

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
@@ -12,13 +14,43 @@ from rango.models import Page
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
+
+    visitor_cookie_handler(request)
+
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
                     'categories': category_list, 'pages': page_list}
     return render(request, 'rango/index.html', context=context_dict)
 
 
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
+# Updated the function definition
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Nick Kalynovskyi'}
+    visitor_cookie_handler(request)
+    context_dict = {'boldmessage': 'This tutorial has been put together by Nick Kalynovskyi',
+                    'visits': request.session['visits']}
     return render(request, 'rango/about.html', context=context_dict)
 
 
